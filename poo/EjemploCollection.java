2d6e14706bdb7f846561809c55763d7f65d77016
AndresRodriguezSomoza <104658111+AndresRodriguezSomoza@users.noreply.github.com>
2025-08-21 22:18:47 -0600

    Add files via upload

---
+++ poo/EjemploCollection.java
@@ -0,0 +1,22 @@
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class EjemploCollection {
+    public static void main(String[] args) {
+        
+        // Declaramos la coleccion (agregamos una lista)
+        Collection<String> nombres = new ArrayList<>();
+
+        // Agregar elementos
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Carlos");
+
+        System.out.println("Nombres: " + nombres);
+
+        // Eliminar un elemento
+        nombres.remove("Carlos");
+
+        System.out.println("Después de eliminar a Carlos: " + nombres);
+    }
+}
